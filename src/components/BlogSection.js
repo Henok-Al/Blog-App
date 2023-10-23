a80d50f7b75de8fe47d3aa8c7ecd4707368671e4
Henok Aemu <henok.gebresenbet@gmail.com>
2023-10-23 10:10:14 -0700

    fetch data and display to the client

---
+++ src/components/BlogSection.js
@@ -0,0 +1,52 @@
+import React from "react";
+import FontAwesome from "react-fontawesome";
+import { Link } from "react-router-dom";
+import { excerpt } from "../utility";
+
+const BlogSection = ({ blogs, user }) => {
+  return (
+    <div>
+      <div className="blog-heading text-start py-2 mb-4">Daily Blogs</div>
+      {blogs?.map((item) => (
+        <div className="row pb-4" key={item.id}>
+          <div className="col-md-5">
+            <div className="hover-blogs-img">
+              <div className="blogs-img">
+                <img src={item.imgUrl} alt={item.title} />
+                <div></div>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-7">
+            <div className="text-start">
+              <h6 className="category catg-color">{item.category}</h6>
+              <span className="title py-2">{item.title}</span>
+              <span className="meta-info">
+                <p className="author">{item.author}</p>
+                {item.timestamp.toDate().toDateString()}
+              </span>
+            </div>
+            <div className="short-description">
+              {excerpt(item.description, 120)}
+            </div>
+            <button className="btn btn-read">Read More</button>
+            <div style={{ float: "right" }}>
+              <FontAwesome
+                name="trash"
+                style={{ margin: "15px", cursor: "pointer" }}
+                size="2x"
+              />
+              <FontAwesome
+                name="edit"
+                style={{ cursor: "pointer" }}
+                size="2x"
+              />
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default BlogSection;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { db } from "../firebase";
+import {
+  collection,
+  deleteDoc,
+  doc,
+  getDocs,
+  limit,
+  onSnapshot,
+  query,
+  orderBy,
+  where,
+  startAfter,
+} from "firebase/firestore";
+import BlogSection from "../components/BlogSection";
 
 const Home = () => {
+  const [loading, setLoading] = useState(true);
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect (() => {
+    const unsub = onSnapshot(
+    collection (db, "blogs"),
+    (snapshot) => {
+    let list = [];
+    snapshot.docs.forEach((doc) => {
+    list.push({id: doc.id, ...doc.data() });
+    });
+    setBlogs (list);
+    },
+    (error) => {
+    console.log(error);
+    }
+    );
+    return () => {
+    unsub();
+    };
+    }, []);
+
+  console.log("blogs", blogs);
+
   return (
     <div className="container-fluid pb-4 pt-4 padding">
       <div className="container padding">
         <div className="row mx-0">
           <h2>Trending</h2>
           <div className="col-md-8">
-            <h2>Blog Section</h2>
+            <BlogSection blogs={blogs}/>
           </div>
           <div className="col-md-3">
             <h2>Tags</h2>
